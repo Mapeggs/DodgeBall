49d1a2ba054f75f5cc96566c3d5c7979d040e9b7
Vansh Shah <117258193+vshah17@users.noreply.github.com>
2024-06-12 17:31:11 -0700

    updates

--- src/Scenes/Gamescene.js
+++ src/Scenes/Gamescene.js
@@ -10,66 +10,67 @@ class Gamescene extends Phaser.Scene {
   }
 
   create() {
-      this.background = this.add.image(240, 160, 'menubackground').setScale(2.5);
+    this.background = this.add.image(240, 160, 'menubackground').setScale(2.5);
 
-      // Court lines
-      this.add.line(0, 0, 490, 0, 490, 800, 0xffffff).setOrigin(0, 0); // Middle line
-      this.add.rectangle(0, 0, 980, 800).setStrokeStyle(2, 0xffffff).setOrigin(0, 0); // Outer rectangle
+    // Court lines
+    this.add.line(0, 0, 490, 0, 490, 800, 0xffffff).setOrigin(0, 0); // Middle line
+    this.add.rectangle(0, 0, 980, 800).setStrokeStyle(2, 0xffffff).setOrigin(0, 0); // Outer rectangle
 
-      // Load characters
-      this.player1 = this.physics.add.sprite(200, 400, 'char1').setScale(2);
-      this.player2 = this.physics.add.sprite(780, 400, 'char2').setScale(2);
+    // Load characters
+    this.player1 = this.physics.add.sprite(200, 400, 'char1').setScale(2);
+    this.player2 = this.physics.add.sprite(780, 400, 'char2').setScale(2);
 
-      // Load balls
-      this.balls = this.physics.add.group({
-          key: 'ball',
-          repeat: 2,
-          setXY: { x: 490, y: 300, stepX: 0, stepY: 200 }
-      });
+    // Set initial angles
+    this.player1.angle = 0;
+    this.player2.angle = 180;
 
-      // Ball physics
-      this.balls.children.iterate((ball) => {
-          ball.setCollideWorldBounds(true)
-          ball.setBounce(0, 0) // No bounce
-          ball.setVelocity(0, 0)
-          ball.body.moves = false // Disable physics initially
-          ball.heldBy = null // No player is holding it
-        })
-        
+    // Load balls
+    this.balls = this.physics.add.group({
+        key: 'ball',
+        repeat: 2,
+        setXY: { x: 490, y: 300, stepX: 0, stepY: 200 }
+    });
 
+    // Ball physics
+    this.balls.children.iterate((ball) => {
+        ball.setCollideWorldBounds(true)
+        ball.setBounce(0, 0) // No bounce
+        ball.setVelocity(0, 0)
+        ball.body.moves = false // Disable physics initially
+        ball.heldBy = null // No player is holding it
+    });
 
-      // Player physics
-      this.player1.setCollideWorldBounds(true);
-      this.player2.setCollideWorldBounds(true);
+    // Player physics
+    this.player1.setCollideWorldBounds(true);
+    this.player2.setCollideWorldBounds(true);
 
-      // Player controls
-      this.cursors = this.input.keyboard.createCursorKeys();
-      this.WASD = this.input.keyboard.addKeys({up: 'W', down: 'S', left: 'A', right: 'D'});
+    // Player controls
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.WASD = this.input.keyboard.addKeys({up: 'W', down: 'S', left: 'A', right: 'D'});
 
-      //player 1 pick-up E and throw SPACE
-      this.pickupKey1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-      this.throwKey1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    //player 1 pick-up E and throw SPACE
+    this.pickupKey1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
+    this.throwKey1 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
-      //Player 2 pick-up K and throw L
-      this.pickupKey2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
-      this.throwKey2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+    //Player 2 pick-up K and throw L
+    this.pickupKey2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.K);
+    this.throwKey2 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
 
-      // Collision detection
-      this.physics.add.overlap(this.balls, this.player1, this.checkOverlap, null, this);
-      this.physics.add.overlap(this.balls, this.player2, this.checkOverlap, null, this);
+    // Collision detection
+    this.physics.add.overlap(this.balls, this.player1, this.checkOverlap, null, this);
+    this.physics.add.overlap(this.balls, this.player2, this.checkOverlap, null, this);
 
-      // Ball collision with players
-      this.physics.add.collider(this.balls, this.player1, (ball, player) => this.hitPlayer(ball, player));
-      this.physics.add.collider(this.balls, this.player2, (ball, player) => this.hitPlayer(ball, player));
+    // Ball collision with players
+    this.physics.add.collider(this.balls, this.player1, (ball, player) => this.hitPlayer(ball, player));
+    this.physics.add.collider(this.balls, this.player2, (ball, player) => this.hitPlayer(ball, player));
 
+    // Score display
+    this.scoreText1 = this.add.text(16, 16, 'Player 1: 0', { fontSize: '32px', fill: '#FFF' });
+    this.scoreText2 = this.add.text(750, 16, 'Player 2: 0', { fontSize: '32px', fill: '#FFF' });
 
-      // Score display
-      this.scoreText1 = this.add.text(16, 16, 'Player 1: 0', { fontSize: '32px', fill: '#FFF' });
-      this.scoreText2 = this.add.text(750, 16, 'Player 2: 0', { fontSize: '32px', fill: '#FFF' });
-
-      this.player1.hasBall = false;
-      this.player2.hasBall = false;
-  }
+    this.player1.hasBall = false;
+    this.player2.hasBall = false;
+}
 
   update() {
       if (this.gameOver) {
@@ -92,19 +93,19 @@ class Gamescene extends Phaser.Scene {
   handleMovement(player, controls, throwKey, spriteKey) {
       if (controls.left.isDown) {
           player.setVelocityX(-160);
-          player.angle = -90;
+          player.angle = 180;
       } else if (controls.right.isDown) {
           player.setVelocityX(160);
-          player.angle = 90;
+          player.angle = 0;
       } else {
           player.setVelocityX(0);
       }
       if (controls.up.isDown) {
           player.setVelocityY(-160);
-          player.angle = 0;
+          player.angle = -90;
       } else if (controls.down.isDown) {
           player.setVelocityY(160);
-          player.angle = 180;
+          player.angle = 90;
       } else {
           player.setVelocityY(0);
       }
@@ -133,7 +134,7 @@ class Gamescene extends Phaser.Scene {
         ball.heldBy = player // Mark ball as held by the player
         player.hasBall = true
         player.ball = ball
-        ball.setVisible(true) // Hide the ball until it is thrown
+        ball.setVisible(true)
       }
     }
     
